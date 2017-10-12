d790f385abed050dd2a78469b00a0857cc95f8e3
Isaac Porta <isaac.porta@gmail.com>
2017-10-12 22:49:06 +0200

    init.py inicia con claves de la base de datos

--- init.py
+++ init.py
@@ -12,20 +12,24 @@ cur = conn.cursor()
 try:
     cur.execute('SELECT key FROM keys WHERE id = 3')
     test = cur.fetchone()[0]
+    cur.execute('SELECT key FROM keys WHERE id = 3 LIMIT 1')
+    user_key = cur.fetchone()[0]
+    cur.execute('SELECT key FROM keys WHERE id = 4 LIMIT 1')
+    user_secret = cur.fetchone()[0]
 except:
     # Si el test falla, lanzamos proceso de autorizacion
     helper.get_auth()
 cur.close()
 
 
-# # Example to get the list of youth players
-# xmldoc = helper.request_resource_with_key(      user_key,
-#                                                user_secret,
-#                                                'youthplayerlist',
-#                                                {
-#                                                 'actionType' : 'details',
-#                                                 'showScoutCall' : 'true',
-#                                                 'showLastMatch' : 'true'
-#                                                }
-#                                               )
-# print(xmldoc)
+# Example to get the list of youth players
+xmldoc = helper.request_resource_with_key(     user_key,
+                                               user_secret,
+                                               'youthplayerlist',
+                                               {
+                                                'actionType' : 'details',
+                                                'showScoutCall' : 'true',
+                                                'showLastMatch' : 'true'
+                                               }
+                                              )
+print(xmldoc)
